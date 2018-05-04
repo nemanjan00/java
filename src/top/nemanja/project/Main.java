c7fb49d96699893bc0d43086d37c1ffeb26fd745
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-05-04 17:29:18 +0200

    HTTP

--- src/top/nemanja/project/Main.java
+++ src/top/nemanja/project/Main.java
@@ -1,7 +1,93 @@
 package top.nemanja.project;
 
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.lang.Runnable;
+import java.io.IOException;
+import java.lang.Thread;
+
+import java.io.PrintWriter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.TreeMap;
+
 class Main {
 	public static void main(String args[]) {
-		System.out.println("Hello world! ");
+		try {
+			new Main();
+		} catch (IOException e) {
+		}
+	}
+
+	public Main() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(8080);
+
+		while(true) {
+			Socket socket = serverSocket.accept();
+
+			Thread thread = new Thread(new ServerThread(socket));
+			thread.run();
+		}
 	}
 }
+
+class ServerThread implements Runnable {
+	Socket socket;
+
+	TreeMap<String, String> arguments = new TreeMap<String, String>();
+
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run(){
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			String request = in.readLine();
+
+			String[] requestParams = request.split(" ");
+
+			if(requestParams.length != 3){
+				socket.close();
+				return;
+			}
+
+			if(!requestParams[2].equals("HTTP/1.1")){
+				socket.close();
+				return;
+			}
+
+
+			String line;
+
+			while((line = in.readLine()) != null && !line.equals("")){
+				System.out.println("<>" + line + "<>");
+
+				String[] argument = line.split(":");
+
+				this.arguments.put(argument[0].toLowerCase(), argument[1]);
+			}
+
+			//TODO: Verify request method
+			//Error 501
+
+			switch(requestParams[0]){
+				case("GET"): 
+					out.println("HTTP/1.1 200 OK");
+					out.println("Content-Length: 4\n");
+					out.println("test");
+
+					break;
+				default:
+					socket.close();
+			}
+		} catch(IOException e){
+		
+		}
+	}
+}
+
